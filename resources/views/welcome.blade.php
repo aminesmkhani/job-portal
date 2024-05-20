06686e01224fb38ae9571419eaccf399ef704f57
Amin <amin.esmkhani75@gmail.com>
2024-05-20 10:07:20 +0330

    create ui for component!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,20 @@
 <x-layout>
-    Hello world
+    <section>
+        <h3 class="mb-6">Top Jobs</h3>
+        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+                <div class="self-start text-sm">Laracasts</div>
+                <div class="py-8 font-bold">
+                    <h3>Video Producer</h3>
+                    <p>Full Time - From $60,000</p>
+                </div>
+                <div class="flex justify-between items-center mt-auto">
+                    <div>
+                        <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                        <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                        <a href="" class="bg-white/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
+                    </div>
+                    <img class="rounded-xl" src="http://placehold.it/42/42" alt="">
+                </div>
+        </div>
+    </section>
 </x-layout>
