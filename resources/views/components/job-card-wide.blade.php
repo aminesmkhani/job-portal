60e130e5ea2ea5595a1cccf3ab3d8c6f9cb6dfc4
Amin <amin.esmkhani75@gmail.com>
2024-05-21 12:37:09 +0330

    complete job card wide!

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -2,10 +2,10 @@
     <div class="">
         <img class="rounded-xl" src="http://picsum.photos/seed/{{rand(0, 100000)}}/100/100" alt="">
     </div>
-    <div class="flex-1">
+    <div class="flex-1 flex flex-col">
         <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
-        <h3>Video Producer</h3>
-        <p>Full Time - From $60,000</p>
+        <h3 class="font-bold text-xl mt-3">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
     </div>
     <div class="">
         <x-tag>Tag</x-tag>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
     <title>Job Portal</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
-<body class="bg-black text-white">
+<body class="bg-black text-white font-hanken-grotesk">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
