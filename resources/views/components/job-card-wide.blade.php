cc3c0ce72a02f2bc43d660cf240f06e95e930858
Amin <amin.esmkhani75@gmail.com>
2024-05-21 12:11:42 +0330

    create wide job car compnent !

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex gap-x-6">
+    <div class="">
+        <img class="rounded-xl" src="http://picsum.photos/seed/{{rand(0, 100000)}}/100/100" alt="">
+    </div>
+    <div class="flex-1">
+        <a href="#" class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+    <div class="">
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+</div>
