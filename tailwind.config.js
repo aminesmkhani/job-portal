e4db8d7dbae6e8fa45a3f9d4c733efa483b8695b
Amin <amin.esmkhani75@gmail.com>
2024-05-21 12:44:32 +0330

    add custom font size for tailwindcss and use tag system!

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ export default {
         },
         fontFamily:{
             "hanken-grotesk": ["Hanken Grotesk","sans-serif"]
+        },
+        fontSize:{
+            "2xs" : "0.625rem" // 10px
         }
     },
   },
