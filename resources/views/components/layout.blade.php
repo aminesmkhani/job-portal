f8bc3fd37fbf8e344358438d1ae3fecb4782ff43
Amin <amin.esmkhani75@gmail.com>
2024-05-21 12:48:14 +0330

    update google font for same size!

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600&display=swap" rel="stylesheet">
     <title>Job Portal</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
