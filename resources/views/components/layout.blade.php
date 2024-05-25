c2d3cbdb5104eaa951dd7c393066a650c85cc92e
Amin <amin.esmkhani75@gmail.com>
2024-05-25 11:01:49 +0330

    arrange code and  import auth and gust section!

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,28 +12,37 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body class="bg-black text-white font-hanken-grotesk pb-20">
-    <div class="px-10">
-        <nav class="flex justify-between items-center py-4 border-b border-white/10">
-            <div>
-                <a href="/">
-                    <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="logo">
-                </a>
-            </div>
+<div class="px-10">
+    <nav class="flex justify-between items-center py-4 border-b border-white/10">
+        <div>
+            <a href="/">
+                <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="logo">
+            </a>
+        </div>
 
+        <div class="space-x-6 font-bold">
+            <a href="">Job</a>
+            <a href="">Careers</a>
+            <a href="">Salaries</a>
+            <a href="">Companies</a>
+        </div>
+        @auth
+            <div>
+                <a href="/jobs/create">Post a Job</a>
+            </div>
+        @endauth
+
+        @guest
             <div class="space-x-6 font-bold">
-                <a href="">Job</a>
-                <a href="">Careers</a>
-                <a href="">Salaries</a>
-                <a href="">Companies</a>
+                <a href="/register">Sign Up</a>
+                <a href="/login">Login</a>
             </div>
-            <div>
-                <a href="">Post a Job</a>
-            </div>
-        </nav>
+        @endguest
+    </nav>
 
-        <main class="mt-10 max-w-[986px] mx-auto">
-            {{$slot}}
-        </main>
-    </div>
+    <main class="mt-10 max-w-[986px] mx-auto">
+        {{$slot}}
+    </main>
+</div>
 </body>
 </html>
