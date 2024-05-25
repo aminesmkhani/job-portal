32154230a4c93a1ae06abba17abd7a1ebe7928f9
Amin <amin.esmkhani75@gmail.com>
2024-05-25 10:43:11 +0330

    create register flow for user and employer!

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,16 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterUserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Store a newly created resource in storage.
      */
-    public function index()
+    public function store(Request $request)
     {
-        //
+        $userAttributes = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => ['required', 'confirmed', Password::min(6)],
+        ]);
+        $employerAttributes = $request->validate([
+            'employer' => 'required',
+            'logo' => ['required', File::types(['png', 'jpg', 'jpeg', 'webp'])],
+        ]);
+
+        $user = User::create($userAttributes);
+        $logoPath = $request->logo->store('logos');
+
+        $user->employer()->create([
+            'name' => $employerAttributes['employer'],
+            'logo' => $logoPath,
+        ]);
+        Auth::login($user);
+        return redirect('/');
     }
 
     /**
@@ -20,45 +42,5 @@ class RegisterUserController extends Controller
     public function create()
     {
         return view('auth.register');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
