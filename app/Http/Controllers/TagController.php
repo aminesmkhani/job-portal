d75cf2690b896f6a372b4aa4780df2f0a68730c9
Amin <amin.esmkhani75@gmail.com>
2024-05-25 11:18:18 +0330

    create relation tags with job!

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    public function __invoke()
+    {
+        // jobs for this tag!
+        return view('results', compact('jobs'));
+    }
 }
