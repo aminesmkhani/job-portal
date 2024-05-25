d6ede165b2e31f5ee4ec62c6044c03800d745243
Amin <amin.esmkhani75@gmail.com>
2024-05-25 10:00:06 +0330

    create authentication route!

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,10 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[JobController::class,'index']);
+
+Route::get('/register',[RegisterUserController::class,'create']);
+Route::post('/register',[RegisterUserController::class,'store']);
+
+Route::get('/login',[SessionController::class,'create']);
+Route::post('/login',[SessionController::class,'store']);
+Route::delete('/logout',[SessionController::class,'destroy']);
