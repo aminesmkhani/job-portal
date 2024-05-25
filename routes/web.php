c7b53c523e2131255b2a1f092733fd5c1d07edf8
Amin <amin.esmkhani75@gmail.com>
2024-05-25 11:52:29 +0330

    edit store route job and add latest return !

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[JobController::class,'index']);
 Route::get('/jobs/create',[JobController::class,'create'])->middleware('auth');
-Route::post('/jobs/create',[JobController::class,'store'])->middleware('auth');
+Route::post('/jobs',[JobController::class,'store'])->middleware('auth');
 Route::get('/search',SearchController::class);
 Route::get('/tags/{tag:name}',TagController::class);
 
